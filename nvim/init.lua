6565277f5becc809a79f68e7d2885a3f70c78f0e
harshelm <harshelm@vayavyalabs.com>
2024-09-24 18:56:53 +0530

    nvim dotfile added

---
+++ nvim/init.lua
@@ -0,0 +1,29 @@
+require("core.options")
+require("core.keymaps")
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+require("lazy").setup({
+	require("plugins.neotree"),
+	require("plugins.colortheme"),
+	require("plugins.bufferline"),
+	require("plugins.lsp"),
+	require("plugins.lualine"),
+	require("plugins.telescope"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformatting"),
+	require("plugins.alpha"),
+	require("plugins.indent-blankline"),
+	require("plugins.comment"),
+})
+-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,87 @@
+return {
+  -- LSP Configuration & Plugins
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    -- Automatically install LSPs and related tools to stdpath for neovim
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+    -- Useful status updates for LSP
+    {
+      'j-hui/fidget.nvim',
+      tag = 'v1.4.0',
+      opts = {
+        progress = {
+          display = {
+            done_icon = '✓', -- Icon shown when all LSP progress tasks are complete
+          },
+        },
+        notification = {
+          window = {
+            winblend = 0, -- Background color opacity in the notification window
+          },
+        },
+      },
+    },
+
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    -- Your existing LSP configuration...
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+    -- Rest of your LSP configuration...
+
+    -- Set up nvim-cmp
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+      sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      },
+    })
+
+    -- Your existing server setup...
+  end,
+}
