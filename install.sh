a2433cd2c86bfb7c1ef9f759d8e694c245fbb7f2
harshelm <harshelm@vayavyalabs.com>
2024-09-26 20:49:45 +0530

    neotree width changed

--- install.sh
+++ install.sh
@@ -5,12 +5,21 @@ DOWNLOAD_DIR="temp_files/"
 DEST_DIR="$HOME/.config/"
 ZIP_FILE="$DOWNLOAD_DIR/main.zip"
 BACKUP_DIR="$HOME/nvim_backup"
+FONT_DIR="$HOME/.local/share/fonts/"
+FONT_ZIP="MartianMono.zip"
 
-#Ensure the backup directory exists
+# Ensure the backup directory exists
 mkdir -p "$BACKUP_DIR"
 
-# Ensure the installation directory exists
+# Ensure the installation and font directories exist
 mkdir -p "$DOWNLOAD_DIR"
+mkdir -p "$FONT_DIR"
+
+# Install MartianMono fonts
+echo "Installing MartianMono fonts..."
+curl -L "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/$FONT_ZIP" -o "$FONT_ZIP"
+unzip "$FONT_ZIP" -d "$FONT_DIR"
+fc-cache -f -v "$FONT_DIR"
 
 # Backup existing Neovim configuration if it exists
 if [ -d "$DEST_DIR/nvim" ]; then
@@ -25,10 +34,9 @@ echo "Unzipping the dotfile..."
 unzip "$ZIP_FILE" -d "$DOWNLOAD_DIR"
 
 echo "Copying..."
-cp -r "$DOWNLOAD_DIR"/dotfiles-main/nvim/ "$DEST_DIR"
+cp -r "$DOWNLOAD_DIR/dotfiles-main/nvim/" "$DEST_DIR"
 
 echo "Cleaning up..."
-rm -rf "$DOWNLOAD_DIR" "$ZIP_FILE"
+rm -rf "$DOWNLOAD_DIR" "$ZIP_FILE" "$FONT_ZIP"
 
 echo "Installation Done!"
-
