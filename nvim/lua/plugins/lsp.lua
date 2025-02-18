328007370e063a06605381123da9dbac3ac87268
herschel21 <harshel.m21@gmail.com>
2025-02-18 22:24:24 +0530

    fixed keymap issue On branch release-1.1 Your branch is up to date with 'origin/release-1.1'.
    Changes to be committed:
    modified:   nvim/lua/plugins/codium.lua
    modified:   nvim/lua/plugins/lsp.lua
    modified:   nvim/lua/plugins/nvim-comp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -67,7 +67,12 @@ return {
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     local servers = {
-      clangd = {},
+      clangd = {
+        cmd = {
+          'clangd',
+          '--offset-encoding=utf-16',
+        },
+      },
       -- gopls = {},
       pyright = {},
       -- rust_analyzer = {},
