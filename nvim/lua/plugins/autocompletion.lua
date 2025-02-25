aefe2a3c3dcbfedf73b31be5ac971cfb0e25300c
herschel21 <harshel.m21@gmail.com>
2025-02-25 17:38:44 +0530

    removed all shitty things On branch minimal-no-shit Your branch is up to date with 'origin/minimal-no-shit'.

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -23,9 +23,6 @@ return { -- Autocompletion
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-
-		-- Adds a number of user-friendly snippets
-		"rafamadriz/friendly-snippets",
 	},
 	config = function()
 		local cmp = require("cmp")
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -15,6 +15,14 @@ return {
     'hrsh7th/cmp-nvim-lsp',
   },
   config = function()
+    -- Disable diagnostics (inline errors and warnings)
+    vim.diagnostic.config({
+      virtual_text = false, -- Disable inline diagnostic messages
+      signs = false,        -- Disable gutter signs
+      underline = false,    -- Disable error underlining
+      update_in_insert = false,
+    })
+
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
@@ -32,6 +40,11 @@ return {
         map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
         map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        
+        -- Add a keybinding to show diagnostics in float window if needed
+        map('<leader>e', vim.diagnostic.open_float, 'Show [E]rror Diagnostics')
+        map('<leader>q', vim.diagnostic.setloclist, 'Show Diagnostic [Q]uicklist')
+        
         local client = vim.lsp.get_client_by_id(event.data.client_id)
         if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
@@ -67,7 +80,12 @@ return {
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
     local servers = {
-      clangd = {},
+      clangd = {
+        cmd = {
+          'clangd',
+          '--offset-encoding=utf-16',
+        },
+      },
       -- gopls = {},
       pyright = {},
       -- rust_analyzer = {},
