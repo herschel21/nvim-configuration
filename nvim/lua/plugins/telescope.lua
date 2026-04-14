f9e493fec664c48f3bca9252f3a3f97c9bcb51b8
herschel21 <harshel.m21@gmail.com>
2026-04-14 12:27:54 +0530

    fix telescope issue
    On branch release-1.1
    Your branch is up to date with 'origin/release-1.1'.
    
    Changes to be committed:
    modified:   nvim/lazy-lock.json
    modified:   nvim/lua/plugins/telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
+	-- branch = "0.1.x", -- REMOVED to fix ft_to_lang error with recent nvim-treesitter
 	cmd = "Telescope",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
